e9450149af639c79e71b6787ff84f1bc18b65c14
sejun2 <qpfjf56@gmail.com>
2024-10-30 23:56:45 +0900

    feat named di

--- src/main/kotlin/di/SesacServiceLocator.kt
+++ src/main/kotlin/di/SesacServiceLocator.kt
@@ -4,15 +4,28 @@ import java.lang.reflect.Type
 import java.rmi.NoSuchObjectException
 
 object SesacServiceLocator {
+    const val DEFAULT_NAME = "\$\$DEFAULT"
 
-    val diCache = HashMap<Type, Any>()
+
+    val diCache = HashMap<Type, HashMap<String, Any>>()
 
     inline fun <reified T : Any> registerSingleton(instance: T) {
-        diCache.put(T::class.java, instance)
+        diCache.put(T::class.java, hashMapOf<String, Any>(DEFAULT_NAME to instance))
+    }
+
+    inline fun <reified T : Any> registerSingleton(instance: T, instanceName: String) {
+        diCache.put(T::class.java, hashMapOf<String, Any>(instanceName to instance))
     }
 
     inline fun <reified T : Any> get(): T {
-        val res = diCache.get(T::class.java) ?: throw NoSuchObjectException("${T::class.java} is not registered")
+        val res = diCache.get(T::class.java)?.get(DEFAULT_NAME)
+            ?: throw NoSuchObjectException("${T::class.java} is not registered")
+        return res as T
+    }
+
+    inline fun <reified T : Any> get(instanceName: String): T {
+        val res = diCache.get(T::class.java)?.get(instanceName)
+            ?: throw NoSuchObjectException("${T::class.java} with $instanceName is not registered")
         return res as T
     }
 
