c08ee8b6f47330b38be79464d1da9d0599fc12cd
sejun2 <qpfjf56@gmail.com>
2024-10-30 23:57:36 +0900

    refactor to call provide di

--- src/main/kotlin/MainApplication.kt
+++ src/main/kotlin/MainApplication.kt
@@ -1,62 +1,9 @@
-import data.datasource.SesacOrderDataSource
-import data.repository.SesacRestaurantOrderRepositoryImpl
-import data.repository.SesacRestaurantPaymentRepositoryImpl
-import data.repository.SesacRestaurantSalesRepositoryImpl
-import di.SesacServiceLocator
-import domain.repository.ISesacRestaurantOrderRepository
-import domain.repository.ISesacRestaurantPaymentRepository
-import domain.repository.ISesacRestaurantSalesRepository
+import di.DIProvider
 import presentation.*
-import presentation.viewmodel.OrderViewModel
-import presentation.viewmodel.PaymentViewModel
-import presentation.viewmodel.SalesViewModel
 
 class MainApplication {
     fun run() {
-        SesacServiceLocator.registerSingleton<SesacOrderDataSource>(SesacOrderDataSource())
-
-        SesacServiceLocator.registerSingleton<ISesacRestaurantSalesRepository>(
-            SesacRestaurantSalesRepositoryImpl(
-                SesacServiceLocator.get<SesacOrderDataSource>()
-            )
-        )
-        SesacServiceLocator.registerSingleton<ISesacRestaurantPaymentRepository>(
-            SesacRestaurantPaymentRepositoryImpl(
-                SesacServiceLocator.get<SesacOrderDataSource>()
-            )
-        )
-        SesacServiceLocator.registerSingleton<ISesacRestaurantOrderRepository>(
-            SesacRestaurantOrderRepositoryImpl(
-                SesacServiceLocator.get<SesacOrderDataSource>()
-            )
-        )
-
-        SesacServiceLocator.registerSingleton<OrderViewModel>(OrderViewModel(orderRepository = SesacServiceLocator.get<ISesacRestaurantOrderRepository>()))
-        SesacServiceLocator.registerSingleton<PaymentViewModel>(
-            PaymentViewModel(
-                salesRepository = SesacServiceLocator.get<ISesacRestaurantSalesRepository>(),
-                paymentRepository = SesacServiceLocator.get<ISesacRestaurantPaymentRepository>()
-            )
-        )
-        SesacServiceLocator.registerSingleton<SalesViewModel>(SalesViewModel(salesRepository = SesacServiceLocator.get<ISesacRestaurantSalesRepository>()))
-
-        SesacServiceLocator.registerSingleton<HomeScreen>(HomeScreen())
-        SesacServiceLocator.registerSingleton<OrderScreen>(
-            OrderScreen(
-                orderViewModel = SesacServiceLocator.get<OrderViewModel>()
-            )
-        )
-        SesacServiceLocator.registerSingleton<PaymentScreen>(
-            PaymentScreen(
-                viewModel = SesacServiceLocator.get<PaymentViewModel>()
-            )
-        )
-        SesacServiceLocator.registerSingleton<SalesScreen>(
-            SalesScreen(
-                salesViewModel = SesacServiceLocator.get<SalesViewModel>()
-            )
-        )
-
+        DIProvider.provide()
         ConsoleController().start()
     }
 }
