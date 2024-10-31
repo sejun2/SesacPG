42d499bf9cfd18f89ac8e3be99373b8f2ca93059
sejun2 <qpfjf56@gmail.com>
2024-10-31 11:07:51 +0900

    add named singleton test

--- src/test/kotlin/di/SesacServiceLocatorTest.kt
+++ src/test/kotlin/di/SesacServiceLocatorTest.kt
@@ -3,6 +3,7 @@ package di
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import presentation.HomeScreen
 import java.rmi.NoSuchObjectException
 
@@ -14,7 +15,7 @@ class SesacServiceLocatorTest {
     }
 
     @Test
-    fun `when try to get same type instance, then the gets will be same`() {
+    fun `given default named singleton instances, when try to get same type instance, then the gets will be same`() {
         SesacServiceLocator.registerSingleton(HomeScreen())
 
         val h1 = SesacServiceLocator.get<HomeScreen>()
@@ -24,7 +25,28 @@ class SesacServiceLocatorTest {
     }
 
     @Test
-    fun `when try to get unregistered instance, then throws NoSuchObjectException`(){
+    fun `given custom named singleton instances, when try to get same named instance, then the gets will be same`() {
+        SesacServiceLocator.registerSingleton(HomeScreen(), "home")
+
+        val h1 = SesacServiceLocator.get<HomeScreen>("home")
+        val h2 = SesacServiceLocator.get<HomeScreen>("home")
+
+        assertEquals(h1.hashCode(), h2.hashCode())
+    }
+
+    @Test
+    fun `given custom named singleton instances, when try to get same type instance without name param, then throws NoSuchObjectException`() {
+        // register named singleton instance
+        SesacServiceLocator.registerSingleton(HomeScreen(), "home")
+
+        assertThrows<NoSuchObjectException> {
+            // belows try to get default named instances
+            val h1 = SesacServiceLocator.get<HomeScreen>()
+        }
+    }
+
+    @Test
+    fun `when try to get unregistered instance, then throws NoSuchObjectException`() {
         org.junit.jupiter.api.assertThrows<NoSuchObjectException> {
             val h1 = SesacServiceLocator.get<HomeScreen>()
         }
